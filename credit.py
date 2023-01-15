8fb3d86a8849203bf82762d44017035e9e27b7cf
humamchoudhary <70004653+humamchoudhary@users.noreply.github.com>
2023-01-15 18:58:45 +0500

    imei

--- credit.py
+++ credit.py
@@ -3,10 +3,20 @@ def parse_int(num):
     return [int(i) for i in str(num)]
 
 
-def validation(c_num):
+def cc_validaiton(c_num):
     if len(str(c_num)) != 16:
         return False
-    num = parse_int(c_num)
+    validation(c_num)
+
+
+def imei_validaiton(imei_num):
+    if len(str(imei_num)) != 15:
+        return False
+    validation(imei_num)
+
+
+def validation(num):
+    num = parse_int(num)
     odd_digits = num[-1::-2]
     even_digits = num[-2::-2]
     odd_sum = sum(odd_digits)
@@ -15,13 +25,14 @@ def validation(c_num):
         if i*2 > 9:
             even_sum += int(str(i*2)[0]) + int(str(i*2)[1])
         else:
-            even_sum+=(i*2)
-    if (odd_sum+even_sum)%10==0:
+            even_sum += (i*2)
+    if (odd_sum+even_sum) % 10 == 0:
         return True
     else:
         return False
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     print(validation(5590490208098960))
     print(validation("661828289361090"))
+    print(validation("351162962088848"))
--- front.py
+++ front.py
@@ -3,7 +3,7 @@ from flet import (colors, AppBar, Text, Column,
 import flet
 
 from flet.buttons import RoundedRectangleBorder
-from credit import validation
+from credit import imei_validaiton, cc_validaiton
 from isbn import isbn10_validation, isbn13_validation
 
 BG_COLOR = colors.WHITE
@@ -34,6 +34,9 @@ def main_page(page):
 
     def isbn13_route(e):
         isbn13_page((page))
+
+    def imei_route(e):
+        imei_val_page((page))
     page.clean()
 
     def back(e):
@@ -45,6 +48,8 @@ def main_page(page):
     APPBAR.title = Text("Validation App")
     cc_btn = ElevatedButton("Credit Card Validation", color=TXT_COLOR, on_click=cc_route,
                             style=ButtonStyle(shape=RoundedRectangleBorder(radius=5)))
+    imei_btn = ElevatedButton("IMEI Validation", color=TXT_COLOR, on_click=imei_route,
+                              style=ButtonStyle(shape=RoundedRectangleBorder(radius=5)))
     isbn10_btn = ElevatedButton("ISBN-10 Validation", color=TXT_COLOR, on_click=isbn10_route,
                                 style=ButtonStyle(shape=RoundedRectangleBorder(radius=5)))
     isbn13_btn = ElevatedButton("ISBN-13 Validation",  color=TXT_COLOR, on_click=isbn13_route,
@@ -52,6 +57,7 @@ def main_page(page):
     page.add(Column([
         Row([
             cc_btn,
+            imei_btn,
             isbn10_btn,
             isbn13_btn
         ], alignment="center")
@@ -64,13 +70,13 @@ def isbn13_page(page):
         confirmation_txt.value = None
         if cc_in.value:
             if isbn13_validation(str(cc_in.value)):
-                confirmation_txt.value = "The card is valid"
+                confirmation_txt.value = "The isbn-13 is valid"
                 cc_in.error_text = None
             else:
-                cc_in.error_text = "Invalid card try again!"
+                cc_in.error_text = "Invalid isbn-13 try again!"
 
         else:
-            cc_in.error_text = "Please enter a card number fields"
+            cc_in.error_text = "Please enter a isbn-13 number"
         page.update()
 
     page.clean()
@@ -109,13 +115,13 @@ def isbn10_page(page):
         confirmation_txt.value = None
         if cc_in.value:
             if isbn10_validation(str(cc_in.value)):
-                confirmation_txt.value = "The card is valid"
+                confirmation_txt.value = "The isbn-10 is valid"
                 cc_in.error_text = None
             else:
-                cc_in.error_text = "Invalid card try again!"
+                cc_in.error_text = "Invalid isbn-10 try again!"
 
         else:
-            cc_in.error_text = "Please enter a card number fields"
+            cc_in.error_text = "Please enter a isbn-10 number"
         page.update()
 
     page.clean()
@@ -154,7 +160,7 @@ def card_val_page(page):
         confirmation_txt.value = None
 
         if cc_in.value:
-            if validation(int(cc_in.value)):
+            if cc_validaiton(int(cc_in.value)):
                 confirmation_txt.value = "The card is valid"
                 cc_in.error_text = None
             else:
@@ -173,6 +179,57 @@ def card_val_page(page):
         # fit="contain",
     ), width=120, height=80)
     cc_label = Text("Enter Credit Card Number: ", color=TXT_COLOR)
+    cc_in = TextField(label="", color=TXT_COLOR, border_color=TXT_COLOR, focused_color=TXT_COLOR, width=400,
+                            focused_border_color=TXT_COLOR, focused_bgcolor="white", selection_color="white")
+    confirmation_txt = Text(value="", color="green")
+    submit_btn = ElevatedButton(
+        "Submit", width=100, height=50, color=TXT_COLOR, on_click=submit, style=ButtonStyle(shape=RoundedRectangleBorder(radius=5)))
+    container = Container(content=Column([
+        Row([confirmation_txt], alignment="center"),
+        cc_label,
+        Row([
+            cc_in, imc,
+            submit_btn
+        ], tight=True
+        )
+    ], tight=True),
+        expand=False,
+        # width=580,
+
+        bgcolor=SEC_COLOR,
+        padding=padding.only(top=20, bottom=20, left=20, right=20),
+        alignment=alignment.center,
+        border_radius=7
+    )
+
+    page.add(Row([container], alignment='center'))
+
+
+def imei_val_page(page):
+    def submit(e):
+        cc_in.error_text = None
+        confirmation_txt.value = None
+
+        if cc_in.value:
+            if imei_validaiton(int(cc_in.value)):
+                confirmation_txt.value = "The IMEI is valid"
+                cc_in.error_text = None
+            else:
+                cc_in.error_text = "Invalid IMEI try again!"
+
+        else:
+            cc_in.error_text = "Please enter a IMEI number"
+        page.update()
+
+    page.clean()
+
+    APPBAR.title = Text("IMEI Validation")
+
+    imc = Container(content=Image(
+        src="vc.png",
+        # fit="contain",
+    ), width=120, height=80)
+    cc_label = Text("Enter IMEI Number: ", color=TXT_COLOR)
     cc_in = TextField(label="", color=TXT_COLOR, border_color=TXT_COLOR, focused_color=TXT_COLOR, width=400,
                             focused_border_color=TXT_COLOR, focused_bgcolor="white", selection_color="white")
     confirmation_txt = Text(value="", color="green")
